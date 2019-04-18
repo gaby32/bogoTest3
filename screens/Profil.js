ac898c79f585ed21efe4af99a57833baf3538fc6
gaby32 <39409503+gaby32@users.noreply.github.com>
2019-04-18 09:18:10 -0400

    added Nate screens

--- screens/Profil.js
+++ screens/Profil.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Container} from "native-base";
+import {Container,View} from "native-base";
 
 
 export default class Profil extends Component {
@@ -7,6 +7,30 @@ export default class Profil extends Component {
     render() {
         return (
             <Container>
+
+                <View style={{flex: 1, flexDirection: 'row'}}>
+
+                    {/* Flex box -> En haut a gauche + en dessous */}
+                    <View style={{flex: 1, flexDirection: 'column'}}>
+                        {/* Flex box -> En haut */}
+                        <View style={{width: '100%', height: '50%', backgroundColor: 'steelblue'}} />
+                        {/* Flex box -> En bas */}
+                        <View style={{width: '100%', height: '50%', backgroundColor: 'skyblue'}} />
+                    </View>
+
+
+                    {/* Flex box -> A droite des deux precedentes */}
+                    <View style={{width: '50%', height: '100%', backgroundColor: 'powderblue'}} />
+
+                </View>
+
+                {/* Flex box -> Les deux box du bas */}
+                <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-end'}}>
+                    {/* Flex box -> En haut */}
+                    <View style={{width: '100%', height: '50%', backgroundColor: 'steelblue'}} />
+                    {/* Flex box -> En bas */}
+                    <View style={{width: '100%', height: '50%', backgroundColor: 'skyblue'}} />
+                </View>
 
             </Container>
         );
