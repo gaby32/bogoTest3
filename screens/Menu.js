ac3f612a7ecd4227b32e31d1e9336223759873b4
gaby32 <39409503+gaby32@users.noreply.github.com>
2019-05-07 14:41:01 -0400

    abstraction & axios getAllInfo method

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,8 +1,23 @@
 import React, {Component} from 'react';
 import {Container, Input, Text, View} from "native-base";
+import {getAllInfo} from "../AxiosRequest";
 
 
 export default class Menu extends Component {
+
+
+
+    componentDidMount(){
+        getAllInfo('evenement')
+            .then(json => json.map(result => (
+                {
+                    nom: result.nom,
+                    adresse: result.adresse
+                })))
+            .then(newData => console.log(newData))
+    }
+
+
 
 
 
@@ -17,6 +32,7 @@ export default class Menu extends Component {
                 <Text>
                     {villeNom}
                 </Text>
+
             </Container>
         );
     }
